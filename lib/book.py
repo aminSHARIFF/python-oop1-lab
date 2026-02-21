725029e93bc738ad9d91b2a3ceb013bd089f7d47
aminSHARIFF <amin29536@gmail.com>
2026-02-21 17:12:09 +0300

    Implement Book and Coffee classes with properties and methods

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    """
+    Book class used to represent a book in the bookstore.
+    """
+
+    def __init__(self, title, page_count):
+        # Store the title provided by the user
+        self.title = title
+
+        # Use the property setter to validate page_count
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        # Return the stored page count
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        # Make sure page_count is an integer
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        # Simulate flipping a page
+        print("Flipping the page...wow, you read fast!")
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    """
+    Coffee class used to represent a coffee item in the bookstore.
+    """
+
+    def __init__(self, size, price):
+        # Set size using property validation
+        self.size = size
+
+        # Store price directly
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        # Ensure size is one of the allowed options
+        if value in ["Small", "Medium", "Large"]:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+
+    def tip(self):
+        # Increase price by 1 when tipping
+        self.price += 1
+        print("This coffee is great, here’s a tip!")
